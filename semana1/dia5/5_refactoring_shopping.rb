18cf8068dde58304e8204853d14f2e32f3454308
codeamac13 <codeamac13@gmail.com>
2017-01-13 16:45:01 -0600

    sem1 dia5 5_refactoring_shopping.rb

---
+++ semana1/dia5/5_refactoring_shopping.rb
@@ -0,0 +1,18 @@
+=begin
+def shopping(list)
+  basket_shopping = nil
+  if basket_shopping == nil
+    basket_shopping = list
+  end
+end
+
+#test
+p shopping(["eggs", "milk", "bread", "orange juice"]) == "eggs, milk, bread, orange juice"
+=end
+def shopping(list)
+  basket_shopping = nil
+  basket_shopping = list.join(", ") if basket_shopping.nil?
+end
+
+#test
+p shopping(["eggs", "milk", "bread", "orange juice"]) == "eggs, milk, bread, orange juice"
