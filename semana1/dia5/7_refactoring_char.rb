91c5bf72598470aa52c09809b2ad868be6b6a99d
codeamac13 <codeamac13@gmail.com>
2017-01-13 17:42:48 -0600

    7_refactoring_char.rb sem1 dia5

---
+++ semana1/dia5/7_refactoring_char.rb
@@ -0,0 +1,14 @@
+=begin
+def char
+  chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+  char_by_char = []
+  chars.each do |char|
+     char_by_char << char
+  end
+  char_by_char.join(", ")
+end
+
+#test
+p char == "A, B, C, D, E, F, G, H, I, J, K"
+=end
+def char; ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"].join(", ") end; p char
