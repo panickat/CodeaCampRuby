61f123cee994d6560903716905d9ab7ca823124d
panic <panic@iMac-de-Cristian.local>
2017-01-17 10:49:42 -0600

    histograma en raiz

---
+++ histrograma_deun_str.rb
@@ -0,0 +1,9 @@
+puts "Texto, por favor: "
+texto = gets.chomp
+
+palabras = texto.split(" ")
+frecuencias = Hash.new(0)
+palabras.each { |palabra| frecuencias[palabra] += 1 }
+frecuencias = frecuencias.sort_by {|a, b| b }
+frecuencias.reverse!
+frecuencias.each { |palabra, frecuencia| puts palabra + " " + frecuencia.to_s }
